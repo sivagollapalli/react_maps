2f4ffea0b14b22f1e85f362cf3f9a75f21497936
sivagollapalli <sivagollapalli88@gmail.com>
2020-08-07 09:54:15 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import polygons from '../src/data/sample.json';
+//import polygons from '../src/data/Web_cementries_demo_Map_ploting.json';
+import { Map, Polygon, GoogleApiWrapper } from 'google-maps-react';
 
-function App() {
+const MapContainer = (props) => {
+  let dataPoints = polygons.features.map((polygon, index) => {
+    return polygon.geometry.coordinates[0].map((points, _) => (
+      { "lng": points[0], "lat": points[1]}
+    ))
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map google={props.google}
+        style={{width: '100%', height: '100%', position: 'relative'}}
+        className={'map'}
+        initialCenter={{
+          lng: -122.448977511080727,
+          lat: 37.676151431885145
+        }}
+        zoom={20}>
+        {
+          dataPoints.map((points, index) => (
+            <Polygon
+              key={index}
+              paths={points}
+              strokeColor="#0000FF"
+              strokeOpacity={0.8}
+              strokeWeight={1}
+              fillColor="#0000FF"
+              fillOpacity={0.35} />
+          ))
+        }
+        
+    </Map>
   );
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyAGDlju5CrE5Nz9id9lM4TnPYKWQUTpjww")
+})(MapContainer)
